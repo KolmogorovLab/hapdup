5494d2a747f5e20a53dea14da1a46298b945c96d
Mikhail Kolmogorov <fenderglass@gmail.com>
2021-10-18 17:56:48 +0000

    pipeline installation

---
+++ setup.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+
+import os
+import sys
+import subprocess
+import shutil
+
+try:
+    import setuptools
+except ImportError:
+    sys.exit("setuptools package not found. "
+             "Please use 'pip install setuptools' first")
+
+from setuptools import setup
+
+# Make sure we're running from the setup.py directory.
+script_dir = os.path.dirname(os.path.realpath(__file__))
+if script_dir != os.getcwd():
+    os.chdir(script_dir)
+
+from hap_dup.__version__ import __version__
+
+
+setup(name="hap_dup",
+      version=__version__,
+      description="Pipeline to convert a haploid assembly into diploid",
+      url="https://github.com/fenderglass/HapDup",
+      author="Mikhail Kolmogorov",
+      author_email = "fenderglass@gmail.com",
+      license="BSD-3-Clause",
+      packages=["hap_dup"],
+      entry_points={"console_scripts": ["hap_dup = hap_dup.main:main"]}
+      )
+
