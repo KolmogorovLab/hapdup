7363ac370b341d244318fdf7a425fc67d73291e3
Mikhail Kolmogorov <fenderglass@gmail.com>
2021-11-16 01:02:44 +0000

    updated model paths

--- hapdup.py
+++ hapdup.py
@@ -16,9 +16,9 @@ if sys.version_info < (3,):
 hap_dup_root = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, hap_dup_root)
 
-os.environ["PEPPER_MODEL"] = os.path.join(hap_dup_root, "pepper_models", "PEPPER_VARIANT_R941_ONT_V5.pkl")
-os.environ["MARGIN_MODEL"] = os.path.join(hap_dup_root, "submodules", "margin", "params", "misc",
-                                          "allParams.ont_haplotag.sv.json")
+os.environ["PEPPER_MODEL"] = os.path.join(hap_dup_root, "pepper_models", "PEPPER_VARIANT_ONT_R941_GUPPY5_SUP_V6.pkl")
+os.environ["MARGIN_MODEL"] = os.path.join(hap_dup_root, "submodules", "margin", "params", "phase",
+                                          "allParams.haplotag.ont-r94g507.incSupAln.json")
 
 #entry point
 from hapdup.main import main
